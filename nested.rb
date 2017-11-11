1c3e766fc586728f030f3e0e7a2a38483da88655
Sherry <sherryptk@gmail.com>
2017-11-11 14:07:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
 		  :known_for => "Ruby",
 		   :languages => ["LISP", "C"]
 		}
-		programmer_hash[:yukihiro_matsumoto]
+		programmer_hash[:yukihiro_matsumoto][:known_for, :languages]
 
 
 end
